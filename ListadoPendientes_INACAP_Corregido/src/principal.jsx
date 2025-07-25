17694b4f611de56814ab0bc91b79b7229106550a
Rich553 <richardolivares3096@gmail.com>
2025-07-25 13:21:03 -0400

    Delete ListadoPendientes_INACAP_Corregido directory

--- ListadoPendientes_INACAP_Corregido/src/principal.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Tareas from './Tareas';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Tareas />
-  </React.StrictMode>
-);
